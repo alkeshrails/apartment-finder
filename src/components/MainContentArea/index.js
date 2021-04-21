7fcb084e61d3e4a9f85834a5c80d80bd4d118e91
Akhilesh <akleshrailsdev@gmail.com>
2021-04-21 14:16:59 +0530

    added search bar design

--- src/components/MainContentArea/index.js
+++ src/components/MainContentArea/index.js
@@ -1,12 +1,17 @@
+import './styles.css'
+import { Search, ArrowDown } from 'react-feather';
 
 function MainContentArea () {
   return (
     <div className="main-content-area">
-      <div className="search-and-filter">
-        Search And Filter
-      </div>
-      <div className="apartments">
-        Apartments List
+      <div className="search-and-filter d-flex p-2 justify-content-between position-sticky">
+        <div className="position-relative">
+          <input className="w-100" />
+          <div className="position-absolute search-icon">
+            <Search color="#41AF69" strokeWidth={2} />
+          </div>
+        </div>
+        <button className="filter-btn d-flex align-items-center">Filters <ArrowDown height="20px" width="20px" /></button>
       </div>
     </div>
   )
