bd2f46a65b3369713c5e0a1fc9ce4346b50e08c5
Akhilesh <akleshrailsdev@gmail.com>
2021-04-21 13:58:21 +0530

    structured code and added header

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header';
+import MainContentArea from '../MainContentArea';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="whole-container">
+      <Header />
+      <MainContentArea />
+    </div>
+  );
+}
+
+export default App;
